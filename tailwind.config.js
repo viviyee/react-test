39df95ad2faa4b249ff99d0ae52875cd46e23a17
Anna <sunayaka2@gmail.com>
2024-03-22 20:00:05 +0630

    Components

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+	content: [
+	  "./index.html",
+	  "./src/**/*.{js,ts,jsx,tsx}",
+	],
+	theme: {
+	  extend: {
+		fontFamily: {
+			sans: ['Roboto', 'sans-serif']
+		},
+		gridTemplateColumns: {
+			'70/30': '70% 28%'
+		}
+	  },
+	},
+	plugins: [],
+  }
