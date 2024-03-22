219ea30ab274dd0c7bd256d1be0cc972304531af
Anna <sunayaka2@gmail.com>
2024-03-22 21:02:57 +0630

    Router

--- src/components/JobCard.jsx
+++ src/components/JobCard.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { FaMapMarker } from 'react-icons/fa'
 
 const JobCard = ({ job }) => {
+	const [fullDesc, setFullDesc] = useState(false)
+
+	let description = fullDesc ? job.description : job.description.substring(0, 100) + '...' 
+
 	return (
 		<div className="bg-white rounded-xl shadow-md relative">
 			<div className="p-4">
@@ -9,9 +16,13 @@ const JobCard = ({ job }) => {
 					<h3 className="text-xl font-bold">{job.title}</h3>
 				</div>
 
-				<div className="mb-5">
-					{job.description}
+				<div className="mb-3">
+					{description}
 				</div>
+
+				<button onClick={() => setFullDesc(prevState => !prevState)} className="text-indigo-500 mb-5 hover:text-indigo-600">
+					{ fullDesc ? 'Show Less' : 'Show More'}
+				</button>
 
 				<h3 className="text-indigo-500 mb-2">{job.salary} / Year</h3>
 
@@ -19,15 +30,15 @@ const JobCard = ({ job }) => {
 
 				<div className="flex flex-col lg:flex-row justify-between mb-4">
 					<div className="text-orange-700 mb-3">
-						<i className="fa-solid fa-location-dot text-lg"></i>
+						<FaMapMarker className="inline text-lg mr-1" />
 						{job.location}
 					</div>
-					<a
-						href={`/job/${job.id}`}
+					<Link
+						to={`/job/${job.id}`}
 						className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
 					>
 						Read More
-					</a>
+					</Link>
 				</div>
 			</div>
 		</div>
